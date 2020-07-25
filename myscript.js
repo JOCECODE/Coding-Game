e06d27d34f9305893fa1e7fd5220eb82af7fcdbf
JOCECODE <ulah57@gmail.com>
2020-07-25 00:19:13 -0700

    timer working as intended

--- myscript.js
+++ myscript.js
@@ -1,4 +1,20 @@
-window.onload = (event) => {
-    console.log('page is fully loaded');
-  };
-  
+// window.onload = (event) => {
+//     console.log('page is fully loaded');
+//   };
+var timeEl = document.querySelector(".time");
+
+var secondsLeft = 60;
+
+function setTime() {
+  var timerInterval = setInterval(function() {
+    secondsLeft --;
+    timeEl.textContent = secondsLeft + " sec. left";
+
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      
+    }
+
+  }, 1000);
+}
+setTime();
