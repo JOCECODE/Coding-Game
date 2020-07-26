a765869b77624b5d0ad3b1662ec209322808da4b
JOCECODE <66341350+JOCECODE@users.noreply.github.com>
2020-07-26 01:09:49 -0700

    accessing array

--- myscript.js
+++ myscript.js
@@ -1,14 +1,17 @@
-var questionsArray = {
-  "Do you lick ass?": ["Sinbad", "Ricky", "Lucy", "Tits"],
-  "Is my ass hairy?": [
-    "Fuck yeah",
-    "Mcnuggets",
-    "I like hippos",
-    "let me hold you",
-  ],
-};
-var correctAnswersArray = ["Sinbad", "Mcnuggets"];
+var questionArray = [
+  {
+    question: "What Year was I born",
+    answers: ["1989", "1986", "1998", "1980"],
+    correctAnswer: "1986",
+  },
+  {
+    question: "Licking dick",
+    answers: ["1989", "2121", "2000", "2000"],
+    correctAnswer: "2000",
+  },
+];
 
+var questionFiller = document.querySelector("#questionBox");
 var timeEl = document.querySelector(".time");
 
 var secondsLeft = 60;
@@ -24,3 +27,7 @@ function setTime() {
   }, 1000);
 }
 setTime();
+
+// put into function
+
+questionFiller.textContent = questionArray[0].question;
