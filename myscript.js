334aba1096451ea2df827e8c5a0f577722b3c1ba
JOCECODE <66341350+JOCECODE@users.noreply.github.com>
2020-07-25 22:45:48 -0700

    added arrays

--- myscript.js
+++ myscript.js
@@ -1,21 +1,26 @@
-// window.onload = (event) => {
-//     console.log('page is fully loaded');
-//   };
+var questionsArray = {
+  "Do you lick ass?": ["Sinbad", "Ricky", "Lucy", "Tits"],
+  "Is my ass hairy?": [
+    "Fuck yeah",
+    "Mcnuggets",
+    "I like hippos",
+    "let me hold you",
+  ],
+};
+var correctAnswersArray = ["Sinbad", "Mcnuggets"];
+
 var timeEl = document.querySelector(".time");
 
 var secondsLeft = 60;
 
 function setTime() {
-  var timerInterval = setInterval(function() {
-    secondsLeft --;
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
     timeEl.textContent = secondsLeft + " sec. left";
 
-    if(secondsLeft === 0) {
+    if (secondsLeft === 0) {
       clearInterval(timerInterval);
-      
     }
-
   }, 1000);
 }
 setTime();
-
